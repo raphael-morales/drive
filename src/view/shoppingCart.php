d7ea6a05b6a15ffa8330bb3cc0125d4161b20bac
UltimatOne <116150495+UltimatOne@users.noreply.github.com>
2024-07-17 16:59:53 +0200

    correction indentation shoppingCarte.php

--- src/view/shoppingCart.php
+++ src/view/shoppingCart.php
@@ -4,37 +4,33 @@
     <?php if (empty($_SESSION['user']['basket'])) { ?>
         <p>Votre panier est vide.</p>
     <?php } else { ?>
-        <?php foreach ($_SESSION['user']['basket'] as $product_ordered) {
-        ?>
+        <?php foreach ($_SESSION['user']['basket'] as $product_ordered) { ?>
             <?php foreach ($this->products as  $product) {
-                if ($product_ordered["product_id"] == $product["product_id"]) {
-            ?>
+                if ($product_ordered["product_id"] == $product["product_id"]) { ?>
                     <form action="" method="post">
                         <div class="card m-3" style="width: 18rem;">
-                            <img src="<?= $product['product_picture'] ?>" class="card-img-top m-auto"
-                                 alt="<?= $product['product_name'] ?>" style="max-height:150px; max-width: 150px;">
+                            <img src="<?= $product['product_picture'] ?>" class="card-img-top m-auto" alt="<?= $product['product_name'] ?>" style="max-height:150px; max-width: 150px;">
                             <div class="card-body" id="<?= $product['product_id'] ?>">
                                 <h5 class="card-title text-truncate"><?= $product['product_name'] ?></h5>
                                 <p class="card-text text-truncate"><?= $product['product_description'] ?></p>
                                 <label for="product_price">Prix : </label>
                                 <p class="card-text"><?= $product['product_price'] ?> €</p>
                                 <span class="card-text">Quantité :
-                                <select name="product_quantity_ordered" id="product_quantity_ordered">
-                                    <?php for ($i = 1; $i <= $product['product_quantity']; $i++) { ?>
-                                        <?php if ($i === $product_ordered["quantity_ordered"]) { ?>
-                                            <option selected value="<?= $i ?>"><?= $i ?></option>
-                                        <?php } else { ?>
-                                            <option value="<?= $i ?>"><?= $i ?></option>
+                                    <select name="product_quantity_ordered" id="product_quantity_ordered">
+                                        <?php for ($i = 1; $i <= $product['product_quantity']; $i++) { ?>
+                                            <?php if ($i === $product_ordered["quantity_ordered"]) { ?>
+                                                <option selected value="<?= $i ?>"><?= $i ?></option>
+                                            <?php } else { ?>
+                                                <option value="<?= $i ?>"><?= $i ?></option>
+                                            <?php } ?>
                                         <?php } ?>
-                                    <?php } ?>
-                                </select>
-                            </span>
+                                    </select>
+                                </span>
                             </div>
                             <button type="submit" name="idProduct" value="<?= $product['product_id'] ?>" class="btn btn-danger">Retirer du panier</button>
                         </div>
                     </form>
-            <?php }
-            } ?>
+            <?php }} ?>
         <?php } ?>
     <?php } ?>
 </div>
